a46345c0ea0f58676fb677dbf33e19641663b93e
Aleksey Dorofeev <aleksej.dorofeev.00@mail.ru>
2023-11-02 23:11:24 +0300

    Добавление контроллера для отправки отзывов, добавление пути к контроллеру в файле urls.py, добавление кадров фильмов и изменение их размера.

--- cinema/movies/urls.py
+++ cinema/movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from movies.views import movies_view, movie_detail
+from movies.views import movies_view, movie_detail, add_review
 
 
 urlpatterns = [
     path('', movies_view),
-    path('<slug:slug>/', movie_detail, name='movie_detail')
+    path('<slug:slug>/', movie_detail, name='movie_detail'),
+    path('review/<int:pk>/', add_review, name='add_review')
 ]
--- cinema/movies/views.py
+++ cinema/movies/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from movies.models import Movie
+from django.shortcuts import render, redirect
+from movies.models import Movie, MovieShots
 
 
 # Create your views here.
+# Контроллер для показа главной страницы.
 def movies_view(request):
     # Делаем вывод только тех фильмов, которые не являются черновиками.
     movies = Movie.objects.filter(draft=False)
@@ -12,6 +13,7 @@ def movies_view(request):
     return render(request, 'movies/movies_list.html', context)
 
 
+# Контроллер для вывода описания к фильму.
 def movie_detail(request, slug):
     # Получаем номер фильма и по нему из базы данных берём информацию.
     movie = Movie.objects.get(url=slug)
@@ -19,4 +21,10 @@ def movie_detail(request, slug):
         'movie': movie
     }
     return render(request, 'movies/movie_detail.html', context)
+
+
+# Контроллер для отправки отзывов.
+def add_review(request, pk):
+    print(request.POST)
+    return redirect('/')
 
