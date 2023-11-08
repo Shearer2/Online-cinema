9ef1a523d547cea853a4240e1802b6dddfb911ac
Aleksey Dorofeev <aleksej.dorofeev.00@mail.ru>
2023-11-08 23:09:43 +0300

    Добавление возможности отображать изображения в админ-панели, а не ссылку на них.

--- cinema/movies/admin.py
+++ cinema/movies/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from movies.models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
 
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    """Категории фильмов."""
+
     list_display = ('id', 'name', 'url')
     # Чтобы переходить в редактирование категории по заданному полю.
     list_display_links = ('name',)
 
 
 class ReviewInline(admin.TabularInline):
+    """Добавление отзывов в редактировании фильмов."""
+
     model = Reviews
     readonly_fields = ('name', 'email')
     extra = 0
@@ -18,7 +23,13 @@ class ReviewInline(admin.TabularInline):
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url', 'draft')
+    """Отображение фильмов."""
+
+    list_display = ('title', 'category', 'url', 'draft', 'get_poster')
+    fields = (
+        'title', 'description', ('poster', 'get_poster'), 'year', 'country', 'directors', 'actors', 'genres',
+        'world_premiere', 'budget', 'fees_in_usa', 'fees_in_world', 'category', 'url', 'draft'
+    )
     # Добавляем фильтрацию фильмов по категории.
     list_filter = ('category',)
     # Делаем поиск по названию фильма.
@@ -35,35 +46,70 @@ class MovieAdmin(admin.ModelAdmin):
     # Чтобы можно было редактировать определённое поле с галочкой, например, черновик, необходимо использовать
     # list_editable.
     list_editable = ('draft',)
+    readonly_fields = ('get_poster',)
+
+    def get_poster(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="60" height="50">')
+
+    get_poster.short_description = "Постер"
 
 
 @admin.register(Reviews)
 class ReviewsAdmin(admin.ModelAdmin):
+    """Отзывы к фильмам."""
+
     list_display = ('name', 'email', 'parent', 'movie')
     readonly_fields = ('name', 'email')
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    """Жанры фильмов."""
+
     list_display = ('name', 'url')
 
 
 @admin.register(MovieShots)
 class MovieShotsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'movie')
+    """Кадры из фильмов."""
+
+    list_display = ('title', 'movie', 'get_image')
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="60" height="50">')
+
+    get_image.short_description = "Изображение"
 
 
 @admin.register(Actor)
 class ActorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'age')
+    """Актёры."""
+
+    # Добавляем сюда имя нашего метода.
+    list_display = ('name', 'age', 'get_image')
+    # Чтобы выводить в редактировании не ссылку на изображение, а само изображение.
+    readonly_fields = ('get_image',)
+
+    # Добавляем метод для вывода изображений, а не просто ссылок, в админ-панели.
+    def get_image(self, obj):
+        # Метод mark_safe выводит html не как строку, а как тег.
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60">')
+
+    # Пишем описание, как будет называться наш столбец.
+    get_image.short_description = "Изображение"
 
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
-    list_display = ('ip', 'star', 'movie')
+    """Рейтинг фильмов."""
+
+    list_display = ('star', 'movie')
 
 
 @admin.register(RatingStar)
 class RatingStarAdmin(admin.ModelAdmin):
+    """Количество звёзд у фильмов."""
+
     list_display = ('value',)
 
--- cinema/movies/apps.py
+++ cinema/movies/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class MoviesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'movies'
+    # Делаем вывод названия таблицы в админ-панели на русском языке.
+    verbose_name = "Фильмы"
