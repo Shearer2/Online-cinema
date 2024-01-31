38ab9e8282b6b56ed49fb525f860534b5d8fd60e
Shearer2 <aleksej.dorofeev.00@mail.ru>
2024-01-31 21:48:59 +0300

    Добавление к модели актёров ссылок на их интервью, удаление из стилей изображений заднего фона и перенос их в html шаблон, отображение видео с интервью актёров и режиссёров на сайте.

--- cinema/movies/models.py
+++ cinema/movies/models.py
@@ -23,6 +23,7 @@ class Actor(models.Model):
     age = models.PositiveSmallIntegerField('Возраст', default=0)
     description = models.TextField('Описание')
     image = models.ImageField('Изображение', upload_to='actors/')
+    link = models.CharField(max_length=160, default='')
 
     def __str__(self):
         return self.name
